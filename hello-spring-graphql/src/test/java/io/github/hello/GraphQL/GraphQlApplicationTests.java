9bfc77531e216719bc834e330d6ae956e92cce0b
Kevin <yangkaibin1992@gmail.com>
2026-07-05 14:50:29 +1000

    init

---
+++ hello-spring-graphql/src/test/java/io/github/hello/GraphQL/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hello.GraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
